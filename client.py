932009d5d1541f85b696913da3d60ad98b6bb798
anthoni Bortolotto Martinelli <antho@outlook.com.br>
2021-12-03 20:06:18 -0300

    reversão

--- client.py
+++ client.py
@@ -1,46 +1,37 @@
 import socket
 import threading
-import sys
-import os
+import sys, os
 
 nome = input('Qual seu nome: ')
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cliente.connect(('localhost', 5000))
 
-
-#Parte de enviar mensagens
-def conCliente():
+#MENSAGENS RECEBIDAS ENTRAM AQUI
+def conServ():
     while True:
         try:
-            #msg = cliente.recv(1024).decode('utf-8')
-            #if msg == "Nome: ":
-                #cliente.send(nome.encode('utf-8'))
-            enviar = input("")
-            if enviar == "y":
-                enviar = input("digite uma mensagem:")
-                cliente.send(enviar.encode('utf-8'))
-            # os.system(msg) #isso ta funcionando mas parece estar com erro
+            msg = cliente.recv(1024).decode('utf-8')
+            if msg == "Nome: ":
+                cliente.send(nome.encode('utf-8'))
+            else:
+                print(msg)
+                os.system(msg) #isso ta funcionando mas parece estar com erro
         except:
             print('Deu Ruim')
             cliente.close()
             break
 
 
-#Quando recebe mensagens
-def conServ():
+#MENSAGENS ENVIADAS ENTRAM AQUI
+def conCliente():
     while True:
-        msg = cliente.recv(1024).decode('utf-8')
-        if msg == "Nome: ":
-            cliente.send(nome.encode('utf-8'))
-        else:
-            print(msg)
-            cliente.send(msg.encode('utf-8'))
-            break
+        msg = input(">>")
+        cliente.send(msg.encode('utf-8'))
 
 
-# UMA THREAD PARA CADA AÇÃO, RECEBER MENSAGEM E ENVIAR
-recebe_thread = threading.Thread(target=conServ)
+#UMA THREAD PARA CADA AÇÃO, RECEBER MENSAGEM E ENVIAR
+recebe_thread = threading.Thread(target = conServ)
 recebe_thread.start()
 
-envia_thread = threading.Thread(target=conCliente)
+envia_thread = threading.Thread(target = conCliente)
 envia_thread.start()
